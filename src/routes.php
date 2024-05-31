8f5d77265266e3c0cddba6afb193c4ce6630aece
Bas Hoppenbrouwers <bashoppie@gmail.com>
2024-05-31 19:21:22 +0200

    Load bootstrap Defined project layout better Created registration route

--- src/routes.php
+++ src/routes.php
@@ -5,4 +5,9 @@ $router->get('/', function() {
     echo "Index";
 });
 
+// User authentication routes
+$router->get('/register', function() {
+    include __DIR__ . '/views/register.view.php';
+});
+
 $router->run();
